ece089a5cb6fd437b0a8f123b0ef0eb124a2d6bc
erangamapa <erangamapa@gmail.com>
2025-03-21 15:11:25 +0800

    Fixed

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { Cog } from "lucide-react";
+import { Cog, X } from "lucide-react";
 import PageTitle from "@/components/page-title";
 import { Button } from "@/components/ui/button";
 import {
@@ -60,6 +60,24 @@ export default function Home() {
                                         <Cog />
                                     </Button>
                                 </NavLink>
+                                <a
+                                    href="https://x.com/AgentHoopz"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <Button size="icon" className="btn-purple">
+                                        <svg
+                                            viewBox="0 0 24 24"
+                                            aria-hidden="true"
+                                            fill="white"
+                                            className="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-lrsllp r-1nao33i r-16y2uox r-8kz0gk"
+                                        >
+                                            <g>
+                                                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
+                                            </g>
+                                        </svg>
+                                    </Button>
+                                </a>
                             </div>
                         </CardFooter>
                     </Card>
