3df8ce0fd7c282fdb01b3454f760f73013711fa2
erangamapa <erangamapa@gmail.com>
2025-03-21 15:17:17 +0800

    Fixed

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { Cog, X } from "lucide-react";
+import { Cog } from "lucide-react";
 import PageTitle from "@/components/page-title";
 import { Button } from "@/components/ui/button";
 import {
